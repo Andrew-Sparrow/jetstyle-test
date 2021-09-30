e8323da772f38cf3d0f26fc41108b91a9c3cea54
Andrew <8911102@gmail.com>
2021-09-30 16:01:33 +0500

    path to mock server was changed

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BACKEND_URL = 'https://heroku-dreamhead-api.herokuapp.com';
+const BACKEND_URL = 'https://jetstyle-api.herokuapp.com';
 const REQUEST_TIMEOUT = 5000;
 
 const token = localStorage.getItem('token') !== null ? localStorage.getItem('token') : '';
