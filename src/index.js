f799e9000872c483ea55e50b2d69b0faa2647b8c
Andrew Pechers <8911102@gmail.com>
2022-06-02 17:05:33 +0600

    изменена версия react и изменен render метод

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import {configureStore} from '@reduxjs/toolkit';
-import {Provider} from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 
-import {getAxiosInstance} from './services/api';
+import { getAxiosInstance } from './services/api';
 import App from './components/app/app';
 import rootReducer from './store/root-reducer';
-import {fetchBooksList} from './store/api-actions';
-import {redirect} from './store/middlewares/redirect';
+import { fetchBooksList } from './store/api-actions';
+import { redirect } from './store/middlewares/redirect';
 import reportWebVitals from './reportWebVitals';
 
 const api = getAxiosInstance();
@@ -25,13 +25,23 @@ const store = configureStore({
 
 store.dispatch(fetchBooksList());
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root'));
+  </React.StrictMode>
+);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
