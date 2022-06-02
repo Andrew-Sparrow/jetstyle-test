4b00b3cb01cb2b1fe9310ca7b1bb4002152744bf
Andrew Pechers <8911102@gmail.com>
2022-06-02 18:45:06 +0600

    удалена кнопка - favorite

--- src/components/book/book.jsx
+++ src/components/book/book.jsx
@@ -3,23 +3,17 @@ import PropTypes from 'prop-types';
 import {useDispatch} from 'react-redux';
 
 import {defaultImg} from '../../const';
-import {addToFavoriteApi, deleteItemApi} from '../../store/api-actions';
+import {deleteItemApi} from '../../store/api-actions';
 
 function Book(props) {
   const {
     id,
     title,
     author,
-    group,
     img,
-    favorite
   } = props;
 
   const dispatch = useDispatch();
-
-  const onFavoriteClick = (evt) => {
-    dispatch(addToFavoriteApi(id, !favorite));
-  };
 
   const onDeleteClick = (evt) => {
     evt.preventDefault();
@@ -35,15 +29,8 @@ function Book(props) {
       <div className="contact__right">
         <p className="contact__data">Book Title: <span className="contact__name">{title}</span></p>
         <p className="contact__data">Author: <span className="contact__name">{author}</span></p>
-        <p className="contact__data">Group: <span className="contact__name">{group}</span></p>
       </div>
       <div className="contact__buttons">
-        <button className="contact__bookmark-button button" type="button" onClick={onFavoriteClick}>
-          <svg className="contact__bookmark-icon" width="30" height="30" style={{stroke: favorite && '#4481c3'}}>
-            <use xlinkHref="#icon-bookmark"></use>
-          </svg>
-          <span className="visually-hidden">Add to bookmarks</span>
-        </button>
         <button className="contact__bookmark-button button" type="button">
           <svg className="contact__edit-icon" width="30" height="30">
             <use xlinkHref="#icon-pencil"></use>
@@ -69,7 +56,6 @@ Book.propTypes = {
     PropTypes.string,
     PropTypes.oneOf([null]),
   ]),
-  "group": PropTypes.string,
 };
 
 export default Book;
